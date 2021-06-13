ed24bc6b22185c65720bafa079ab6320c0f72481
Saransh Kumar <cryptosingh@gmail.com>
2021-06-13 19:06:48 +0530

    initial commit

---
+++ src/main/java/com/saransh/springrecipeapp/SpringRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.saransh.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeAppApplication.class, args);
+    }
+
+}
