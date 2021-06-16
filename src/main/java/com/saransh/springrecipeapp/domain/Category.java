32c00e9d6ffc8518c116cb4febcb4b62a0afd52a
Saransh Kumar <cryptosingh@gmail.com>
2021-06-16 19:35:19 +0530

    Closes #7: Create Category Entity and Associate it with Recipe Entity (@ManyToMany).

---
+++ src/main/java/com/saransh/springrecipeapp/domain/Category.java
@@ -0,0 +1,44 @@
+package com.saransh.springrecipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by CryptoSingh1337 on 6/16/2021
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
