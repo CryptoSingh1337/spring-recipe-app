e622260e28ae48872e30db28fd715a7acba758cb
Saransh Kumar <cryptosingh@gmail.com>
2021-06-16 17:41:09 +0530

    Closes #6: Create Difficulty enum and add it to Recipe Entity.

---
+++ src/main/java/com/saransh/springrecipeapp/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package com.saransh.springrecipeapp.domain;
+
+/**
+ * Created by CryptoSingh1337 on 6/16/2021
+ */
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/saransh/springrecipeapp/domain/Recipe.java
+++ src/main/java/com/saransh/springrecipeapp/domain/Recipe.java
@@ -20,8 +20,10 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo add
-    // private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -93,6 +95,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Set<Ingredient> getIngredients() {
