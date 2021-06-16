9745e10a61cbaa10bcf4f9dc53e13643b7fc8212
Saransh Kumar <cryptosingh@gmail.com>
2021-06-16 16:52:36 +0530

    Closes #3: Create Recipe and Notes Entity and Associate them (@OneToOne).

---
+++ src/main/java/com/saransh/springrecipeapp/domain/Notes.java
@@ -0,0 +1,43 @@
+package com.saransh.springrecipeapp.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by CryptoSingh1337 on 6/16/2021
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
