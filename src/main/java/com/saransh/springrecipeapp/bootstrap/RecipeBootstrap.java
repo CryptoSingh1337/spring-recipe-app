0419d17ea5077085021251020a11574d670748d5
Saransh Kumar <cryptosingh@gmail.com>
2021-06-22 20:34:49 +0530

    Closes #15: Add @Transactional on onApplicationEvent method in RecipeBootstrap class.

--- src/main/java/com/saransh/springrecipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/saransh/springrecipeapp/bootstrap/RecipeBootstrap.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +32,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
         log.debug("Loaded Bootstrap Data");
