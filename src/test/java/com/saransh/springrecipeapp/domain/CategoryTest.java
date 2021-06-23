6a83c91768f5cb5a82ef9860336bff18c1ed7036
Saransh Kumar <cryptosingh@gmail.com>
2021-06-23 16:19:40 +0530

    Closes #17: Create Sample Unit Test for Category class.

---
+++ src/test/java/com/saransh/springrecipeapp/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package com.saransh.springrecipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    private Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long id = 4L;
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
