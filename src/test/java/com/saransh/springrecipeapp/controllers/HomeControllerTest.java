8f2d0a86fda08b9ac6e7028954847d8ca4cd402d
Saransh Kumar <cryptosingh@gmail.com>
2021-06-23 20:06:19 +0530

    Closes #20: Add MockMvc Unit Test for Home Controller.

--- src/test/java/com/saransh/springrecipeapp/controllers/HomeControllerTest.java
+++ src/test/java/com/saransh/springrecipeapp/controllers/HomeControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +16,9 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class HomeControllerTest {
 
@@ -27,6 +32,14 @@ class HomeControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         controller = new HomeController(recipeService);
+    }
+
+    @Test
+    void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
