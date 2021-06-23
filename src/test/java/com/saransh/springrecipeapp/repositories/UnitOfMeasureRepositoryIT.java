203011cb94d7339fc2c9d8ba88824530b60ffd2e
Saransh Kumar <cryptosingh@gmail.com>
2021-06-23 20:19:06 +0530

    Closes #21: Add Integration Test for UnitOfMeasureRepository.

---
+++ src/test/java/com/saransh/springrecipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,31 @@
+package com.saransh.springrecipeapp.repositories;
+
+import com.saransh.springrecipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> optional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", optional.get().getDescription());
+    }
+}
