58d49e2deafeaeca1a8c465301b0595cd72e6dfc
Saransh Kumar <cryptosingh@gmail.com>
2021-07-04 15:46:04 +0530

    Closes #48: Refactor Ingredient Service code in new branch.

--- src/main/java/com/saransh/springrecipeapp/services/IngredientServiceImpl.java
+++ src/main/java/com/saransh/springrecipeapp/services/IngredientServiceImpl.java
@@ -4,15 +4,12 @@ import com.saransh.springrecipeapp.commands.IngredientCommand;
 import com.saransh.springrecipeapp.converters.IngredientCommandToIngredient;
 import com.saransh.springrecipeapp.converters.IngredientToIngredientCommand;
 import com.saransh.springrecipeapp.domain.Ingredient;
-import com.saransh.springrecipeapp.domain.Recipe;
-import com.saransh.springrecipeapp.repositories.RecipeRepository;
-import com.saransh.springrecipeapp.repositories.UnitOfMeasureRepository;
+import com.saransh.springrecipeapp.repositories.IngredientRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Created by CryptoSingh1337 on 7/2/2021
@@ -21,97 +18,39 @@ import java.util.Optional;
 @Service
 public class IngredientServiceImpl implements IngredientService {
 
-    private final RecipeRepository recipeRepository;
+    private final IngredientRepository ingredientRepository;
     private final IngredientToIngredientCommand convertor;
     private final IngredientCommandToIngredient convertorReversed;
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IngredientServiceImpl(RecipeRepository recipeRepository,
-                                 IngredientToIngredientCommand convertor,
-                                 IngredientCommandToIngredient convertorReversed,
-                                 UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.recipeRepository = recipeRepository;
+    public IngredientServiceImpl(
+            IngredientRepository ingredientRepository,
+            IngredientToIngredientCommand convertor,
+            IngredientCommandToIngredient convertorReversed) {
+        this.ingredientRepository = ingredientRepository;
         this.convertor = convertor;
         this.convertorReversed = convertorReversed;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
     public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
-        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
-        if (recipeOptional.isEmpty())
-            log.error("Recipe not found with Id: " + recipeId);
-
-        Recipe recipe = recipeOptional.get();
-        Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients()
-                .stream()
-                .filter(ingredient -> ingredient.getId().equals(ingredientId))
-                .map(convertor::convert).findFirst();
-
-        if (ingredientCommandOptional.isEmpty())
-            log.error("Ingredient not found with Id: " + ingredientId);
-        // TODO: error handling if not found
-        return ingredientCommandOptional.get();
+        return convertor.convert(
+                ingredientRepository.findByRecipeIdAndIngredientId(
+                        recipeId,
+                        ingredientId)
+                .orElseThrow(() -> new RuntimeException("Ingredient not found with Id: " + ingredientId)));
     }
 
     @Override
     @Transactional
     public IngredientCommand saveIngredientCommand(Long recipeId, IngredientCommand command) {
-        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
-        if (recipeOptional.isEmpty()) {
-            // TODO: error handling if not found
-            log.error("Recipe not found with Id: " + recipeId);
-            return new IngredientCommand();
-        }
-        Recipe recipe = recipeOptional.get();
-        Optional<Ingredient> ingredientOptional = recipe.getIngredients().stream()
-                .filter(ingredient -> ingredient.getId().equals(command.getId()))
-                .findFirst();
-
-        if (ingredientOptional.isPresent()) {
-            Ingredient ingredientFound = ingredientOptional.get();
-            ingredientFound.setDescription(command.getDescription());
-            ingredientFound.setAmount(command.getAmount());
-            ingredientFound.setUom(unitOfMeasureRepository
-                    .findById(command.getUnitOfMeasure().getId())
-                    .orElseThrow(() -> new RuntimeException("Uom not found")));
-        } else {
-            recipe.addIngredient(Objects.requireNonNull(convertorReversed.convert(command)));
-        }
-
-        Recipe savedRecipe = recipeRepository.save(recipe);
-        Optional<Ingredient> savedIngredientOptional = savedRecipe.getIngredients().stream()
-                .filter(recipeIngredient -> recipeIngredient.getId().equals(command.getId()))
-                .findFirst();
-
-        if (savedIngredientOptional.isEmpty()) {
-            savedIngredientOptional = savedRecipe.getIngredients().stream()
-                    .filter(ingredient ->  ingredient.getAmount().equals(command.getAmount()) &&
-                            ingredient.getDescription().equals(command.getDescription()) &&
-                            ingredient.getUom().getId().equals(command.getUnitOfMeasure().getId()))
-                    .findFirst();
-        }
-        return convertor.convert(savedIngredientOptional.get());
+        Ingredient savedIngredient = ingredientRepository.save(
+                Objects.requireNonNull(convertorReversed.convert(command)));
+        return convertor.convert(savedIngredient);
     }
 
     @Override
     @Transactional
     public void deleteById(Long recipeId, Long ingredientId) {
-        Recipe recipe = recipeRepository.findById(recipeId)
-                .orElseThrow(() -> new RuntimeException("Recipe not found"));
-        log.debug("Deleting ingredient for Recipe with Id: " + recipeId);
-        Optional<Ingredient> ingredientToBeRemoveOptional = recipe.getIngredients().stream()
-                .filter(ingredient -> ingredient.getId().equals(ingredientId))
-                .findFirst();
-
-        if (ingredientToBeRemoveOptional.isPresent()) {
-            log.debug("Ingredient with Id " + ingredientId + "found");
-            Ingredient ingredientToBeRemove = ingredientToBeRemoveOptional.get();
-            ingredientToBeRemove.setRecipe(null);
-            recipe.getIngredients().remove(ingredientToBeRemove);
-            recipeRepository.save(recipe);
-        } else {
-            throw new RuntimeException("Ingredient not found with Id: " + ingredientId);
-        }
+        ingredientRepository.deleteByIngredientIdAndRecipeId(recipeId, ingredientId);
     }
 }
