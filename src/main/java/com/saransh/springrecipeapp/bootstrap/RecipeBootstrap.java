566566cb8a418fb0debc8c77e17e89d0e74dd022
Saransh Kumar <cryptosingh@gmail.com>
2021-07-18 17:10:59 +0530

    Closes #65: Configure MySQL with Spring boot.

--- src/main/java/com/saransh/springrecipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/saransh/springrecipeapp/bootstrap/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import com.saransh.springrecipeapp.repositories.RecipeRepository;
 import com.saransh.springrecipeapp.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,7 @@ import java.util.List;
  */
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
